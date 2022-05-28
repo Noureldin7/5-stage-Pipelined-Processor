5dc56e25c684c1fe184b54fffe37b2bf73bbc5d9
John Maurice <johnmaurice71@gmail.com>
2022-05-28 15:00:13 +0200

    HDU updates

--- HDU.vhd
+++ HDU.vhd
@@ -17,6 +17,9 @@ ENTITY HDU IS
         r_DE_Imm : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
         r_DE_MEMR : IN STD_LOGIC;
 
+        r_EM_MEMR : IN STD_LOGIC;
+        r_EM_MEMW : IN STD_LOGIC;
+
         w_FD_OpCode : OUT STD_LOGIC_VECTOR(6 DOWNTO 0); --values to fetch decode buffer
         w_FD_RD : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
         w_FD_RT : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
@@ -33,33 +36,49 @@ ENTITY HDU IS
         Imm_2 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); --forwarded val
 
         EN : OUT STD_LOGIC; --PC enable
-        Swap_Hazard : OUT STD_LOGIC; 
-        Load_Use : OUT STD_LOGIC 
+        Swap_Hazard : OUT STD_LOGIC;
+        Load_Use : OUT STD_LOGIC;
+        HLT : OUT STD_LOGIC
     );
 END ENTITY;
 
 ARCHITECTURE HDU_ARCH OF HDU IS
     SIGNAL Load_Use_Signal : STD_LOGIC;
     SIGNAL Swap_Hazard_Signal : STD_LOGIC;
+    SIGNAL HLT_Signal : STD_LOGIC;
 BEGIN
 
     Load_Use_Signal <= '1' WHEN r_DE_MEMR = '1' AND (r_DE_RD = r_FD_RS OR r_DE_RD = r_FD_RT) ELSE
         '0';
     Swap_Hazard_Signal <= '1' WHEN r_FD_OpCode = "001100" ELSE
         '0';
+    HLT_Signal <= '1' WHEN Ins(31 DOWNTO 25) = "000001" ELSE
+        "0";
+
     Load_Use <= Load_Use_Signal;
     Swap_Hazard <= Swap_Hazard_Signal;
+    HLT <= HLT_Signal;
 
     PROCESS IS
     BEGIN
-        IF (Load_Use_Signal = '1') THEN
+        IF (HLT_Signal = '1'|| r_EM_MEMR = '1' || r_EM_MEMW = '1') THEN
             EN <= '0';
 
-            w_DE_OpCode <= (OTHERS => '0');
-            w_DE_RD <= (OTHERS => '0');
-            w_DE_RT <= (OTHERS => '0');
-            w_DE_RS <= (OTHERS => '0');
-            w_DE_Imm <= (OTHERS => '0');
+            w_FD_OpCode (OTHERS => 'X');
+            w_FD_RD <= (OTHERS => 'X');
+            w_FD_RT <= (OTHERS => 'X');
+            w_FD_RS <= (OTHERS => 'X');
+            w_FD_Imm <= (OTHERS => 'X');
+
+            w_DE_OpCode <= r_DE_OpCode;
+            w_DE_RD <= r_DE_RD;
+            w_DE_RT <= r_DE_RT;
+            w_DE_RS <= r_DE_RS;
+            w_DE_Imm <= r_DE_Imm;
+
+        END IF;
+        IF (Load_Use_Signal = '1') THEN
+            EN <= '0';
 
             w_FD_OpCode <= r_FD_OpCode;
             w_FD_RD <= r_FD_RD;
@@ -67,8 +86,20 @@ BEGIN
             w_FD_RS <= r_FD_RS;
             w_FD_Imm <= r_FD_Imm;
 
+            w_DE_OpCode <= (OTHERS => '0');
+            w_DE_RD <= (OTHERS => '0');
+            w_DE_RT <= (OTHERS => '0');
+            w_DE_RS <= (OTHERS => '0');
+            w_DE_Imm <= (OTHERS => '0');
+
         ELSIF (Swap_Hazard_Signal = '1') THEN
             EN <= '0';
+
+            w_FD_OpCode <= "101100";
+            w_FD_RD <= r_FD_RS;
+            w_FD_RT <= (OTHERS => 'X');
+            w_FD_RS <= (OTHERS => 'X');
+            w_FD_Imm <= Imm_2;
 
             w_DE_OpCode <= "101100";
             w_DE_RD <= r_FD_RD;
@@ -76,11 +107,21 @@ BEGIN
             w_DE_RS <= (OTHERS => 'X');
             w_DE_Imm <= Imm_1;
 
-            w_FD_OpCode <= "101100";
-            w_FD_RD <= r_FD_RS;
-            w_FD_RT <= (OTHERS => 'X');
-            w_FD_RS <= (OTHERS => 'X');
-            w_FD_Imm <= Imm_2;
+        ELSE
+            EN <= '1';
+
+            w_FD_OpCode <= r_FD_OpCode;
+            w_FD_RD <= r_FD_RD;
+            w_FD_RT <= r_FD_RT;
+            w_FD_RS <= r_FD_RS;
+            w_FD_Imm <= r_FD_Imm;
+
+            w_DE_OpCode <= r_DE_OpCode;
+            w_DE_RD <= r_DE_RD;
+            w_DE_RT <= r_DE_RT;
+            w_DE_RS <= r_DE_RS;
+            w_DE_Imm <= r_DE_Imm;
+
         END IF;
     END PROCESS;
 
